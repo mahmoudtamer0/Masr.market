1877d89300bf577ed7022765b2b4305ffc997622
mahmoud <mamoidtamer300@gmail.com>
2023-08-02 02:22:44 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import ProductList from "./components/Productlist";
-import About from "./components/About";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import ProductDetails from "./components/ProductDetails";
 import Footer from "./components/Footer";
+import PreLoader from "./components/Preloader";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<><Landing /> <ProductList /></>} />
-          <Route path="/products/:productId" element={<ProductDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
+
+      <div className="main-page">
+        <Router>
+          <Navbar />
+          {/* <PreLoader /> */}
+          <Routes>
+            <Route path="/" element={<><Landing /> <ProductList /></>} />
+            <Route path="/products/:productId" element={<ProductDetails />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
 }
