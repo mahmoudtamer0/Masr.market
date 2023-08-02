58598321de00152f7706f937fd15099ed8ee8ceb
mahmoud <mamoidtamer300@gmail.com>
2023-08-02 21:12:33 +0300

    fixing preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -5,16 +5,16 @@ import './preloader.css'
 const PreLoader = () => {
 
     // use IIFE function to insulate from global namespace
-    (function () {
-        var onload = function () {
-            document.querySelector('.preloader').classList.add('none')
-        };
-        window.addEventListener('load', onload);
-    })();
+    // (function () {
+    //     var onload = function () {
+    //         document.querySelector('.preloader').classList.add('none')
+    //     };
+    //     window.addEventListener('load', onload);
+    // })();
 
-    // window.addEventListener('load', (event) => {
-    //     document.querySelector('.preloader').classList.add('none')
-    // })
+    window.addEventListener('load', (event) => {
+        document.querySelector('.preloader').classList.add('none')
+    })
     return (
         <div className="preloader">
             <div className="custom-loader"></div>
