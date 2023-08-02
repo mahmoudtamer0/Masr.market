5c572d2ee4c1424a9f4cca7affdfa3f0099c7ae7
mahmoud <mamoidtamer300@gmail.com>
2023-08-03 01:12:24 +0300

    app

--- src/App.js
+++ src/App.js
@@ -5,23 +5,42 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import ProductDetails from "./components/ProductDetails";
 import Footer from "./components/Footer";
 import PreLoader from "./components/Preloader";
+import { useEffect, useState, CSSProperties } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500);
+  }, [])
   return (
     <div className="App">
+      {loading ?
+        <div className="preloader">
+          <HashLoader
+            color='#ed8a8a'
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          /></div> : <div >
+          <Router basename="/Masr.market">
+            <Navbar />
+            <PreLoader />
+            <Routes>
+              <Route path="/" element={<><Landing /> <ProductList /></>} />
+              <Route path="/products/:productId" element={<ProductDetails />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </div>}
 
-      <div>
-        <Router basename="/Masr.market">
-          <Navbar />
-          <PreLoader />
-          <Routes>
-            <Route path="/" element={<><Landing /> <ProductList /></>} />
-            <Route path="/products/:productId" element={<ProductDetails />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </div>
-    </div>
+    </div >
   );
 }
 
