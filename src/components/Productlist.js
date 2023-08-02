3a3ed8155846e495bddf5cc9eecb94c3cfa0da04
mahmoud <mamoidtamer300@gmail.com>
2023-08-02 07:11:07 +0300

    fixing error in product list

--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -7,15 +7,13 @@ function ProductList() {
     const [Products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
     const getProducts = () => {
-        fetch(api_url).then((res) => res.json()).then((data) => setProducts(data))
+        fetch(api_url).then((res) => res.json()).then((data) => { setProducts(data) })
     }
     const getcategories = () => {
         fetch("https://btngan-data.onrender.com/catigories")
             .then((res) => res.json())
-            .then((data) => setCategories(data))
+            .then((data) => { setCategories(data) })
     }
-
-    console.log(document.getElementById('bttt'))
 
     const getincategories = (catname) => {
         fetch(`https://btngan-data.onrender.com/${catname}`)
@@ -33,22 +31,22 @@ function ProductList() {
         <div className='product-list'>
             <h2 className="text-center">Our Products</h2>
             <div className="container">
-                <div class="btn-group dropend">
-                    <button id='bttt' type="button" class="down-btn btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="btn-group dropend">
+                    <button id='bttt' type="button" className="down-btn btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         Categories
                     </button>
-                    <ul class="dropdown-menu">
+                    <ul className="dropdown-menu">
                         <li><button className='dropdown-item' onClick={() => getProducts()}>All</button></li>
                         {categories.map(cat => {
                             return (
-                                <li><button className='dropdown-item' onClick={() => getincategories(cat)}>{cat}</button></li>
+                                <li key={cat}><button className='dropdown-item' onClick={() => getincategories(cat)}>{cat}</button></li>
                             )
                         })}
                     </ul>
                 </div>
 
                 <div className="row justify-content-center align-items-center products-box">
-                    {Products.map((product) => {
+                    {Products?.map((product) => {
                         return (
                             <div className="col-lg-3 col-12 col-md-4 col-sm-6" key={product.id}>
                                 <div className="card card-product">
