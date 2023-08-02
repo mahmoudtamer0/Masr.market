08ed3bb56180c07354b14dfd23c568a49d36531c
mahmoud <mamoidtamer300@gmail.com>
2023-08-02 07:10:34 +0300

    edit preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -5,13 +5,11 @@ import './preloader.css'
 const PreLoader = () => {
 
     window.addEventListener('load', (event) => {
-        setTimeout(() => {
-            document.querySelector('.preloader').classList.add('none')
-        }, 1500)
+        document.querySelector('.preloader').classList.add('none')
     })
     return (
         <div className="preloader">
-            <div class="custom-loader"></div>
+            <div className="custom-loader"></div>
         </div>
     );
 };
