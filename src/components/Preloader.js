aa235f3979d5ea280035773160c8b575b84e9128
mahmoud <mamoidtamer300@gmail.com>
2023-08-02 23:58:23 +0300

    fixing preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -12,9 +12,7 @@ const PreLoader = () => {
     //     window.addEventListener('load', onload);
     // })();
 
-    window.addEventListener('load', (event) => {
-        document.querySelector('.preloader').classList.add('none')
-    })
+
     return (
         <div className="preloader">
             <div className="custom-loader"></div>
