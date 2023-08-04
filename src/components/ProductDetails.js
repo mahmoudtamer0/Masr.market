3a4f06ffc8d15c0c7bae4ecead7df91659e96182
mahmoud <mamoidtamer300@gmail.com>
2023-08-05 01:46:10 +0300

    fetch.post product to cart tree

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import './proddet.css'
+import axios from "axios";
 function ProductDetails() {
     const { productId } = useParams();
     const api_url = 'https://btngan-data.onrender.com/products';
@@ -9,6 +10,15 @@ function ProductDetails() {
         fetch(`${api_url}/${productId}`)
             .then(res => res.json()).then(data => setProduct(data))
     }, [])
+
+    const addtoserver = () => {
+        axios.post('https://btngan-data.onrender.com/cart', {
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            image: product.image,
+        })
+    }
     return (
         <div className="container">
 
@@ -17,6 +27,7 @@ function ProductDetails() {
                 <img src={product.image} />
             </div>
             <p> {product.description}</p>
+            <button onClick={() => addtoserver()}>add to cart</button>
         </div>
     )
 }
