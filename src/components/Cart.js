73c8d540c6c13a3b4ca68e8d184b05e946faea29
mahmoud <mamoidtamer300@gmail.com>
2023-08-06 23:52:42 +0300

    edit on cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,70 +9,60 @@ function Cart() {
     useEffect(() => {
         getProductsInCart()
     }, [])
+    let total = 0;
+
+    products.map(prod => { return (total += prod.price) })
+    console.log(total)
 
     const getProductsInCart = () => {
         fetch('https://btngan-data.onrender.com/cart')
             .then(res => res.json())
             .then(data => { setProducts(data) })
     }
-
-    const countplus = () => {
-        if (count < 5) {
-            setCount(count + 1)
-            document.getElementById('minuscounter').classList.remove('disa')
-            document.getElementById('pluscounter').classList.remove('disa')
-        } else if (count >= 5) {
-            document.getElementById('pluscounter').classList.add('disa')
-            return false
-        }
-    }
-
-    const countminus = () => {
-        if (count > 1) {
-            setCount(count - 1)
-            document.getElementById('minuscounter').classList.remove('disa')
-        } else {
-            document.getElementById('pluscounter').classList.remove('disa')
-            document.getElementById('minuscounter').classList.add('disa')
-            return false
-        }
-    }
-
     const removeprod = (prodId) => {
         axios.delete(`https://btngan-data.onrender.com/cart/${prodId}`)
             .then(data => { getProductsInCart() })
     }
 
+
     return (
         <div className="maincart">
             <div className="container">
                 <h2 className="text-center">Cart.</h2>
-                <div className="prods">
-                    {products?.map(product => {
-                        return (
-                            <div className="cartproduct" key={product.id}>
-                                <div className="cartimg">
-                                    <img src={product.image}></img>
-                                </div>
-                                <div>
-                                    <h3>{product.title.slice(0, 40)}..</h3>
-                                    <p>Price:  {product.price}$</p>
-                                    <div className="counterdiv">
-                                        <span>Count: </span>
-                                        <i id="minuscounter" className="fa-solid fa-circle-minus" onClick={() => countminus()}></i>
-                                        <span>{count}</span>
-                                        <i id="pluscounter" className="fa-solid fa-circle-plus" onClick={() => countplus()}></i>
+                <div className="row">
+                    <div className="prods col-7">
+                        {products.length !== 0 ? products.map(product => {
+                            return (
+                                <div className="cartproduct" key={product.id}>
+                                    <div className="cartimg">
+                                        <img src={product.image}></img>
+                                    </div>
+                                    <div className="boxcontent">
+                                        <div>
+                                            <h3>{product.title}</h3>
+                                            <p>Price:  {product.price}$</p>
+                                            <div className="counterdiv">
+                                                <span>Count: </span>
+                                                <select>
+                                                    <option value='1'>1</option>
+                                                    <option value='2'>2</option>
+                                                    <option value='3'>3</option>
+                                                    <option value='4'>4</option>
+                                                    <option value='5'>5</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="basketdiv">
+                                            <i onClick={() => { removeprod(product.id) }} class="fa-regular fa-trash-can"></i>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="basketdiv">
-                                    <i onClick={() => { removeprod(product.id) }} class="fa-regular fa-trash-can"></i>
-                                </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        }) : <div className="noprod"><h4 className="text-center">no products selected</h4></div>}
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
