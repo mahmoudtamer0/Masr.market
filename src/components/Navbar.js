daa24fef49b2cd9a5369615eab1b8b8e64bb0f1d
mahmoud <mamoidtamer300@gmail.com>
2023-08-07 04:02:50 +0300

    n

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import { NavLink } from "react-router-dom";
 import './navbar.css'
-import { faFontAwesome } from "@fortawesome/free-regular-svg-icons";
-function Navbar() {
+import { useState } from "react";
+import { useEffect } from "react";
+function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container">
